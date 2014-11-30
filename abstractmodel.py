4a0ffa390f1ca29a16d8a4de12d818164b5f7c77
Tayamarn <max@goldenforests.ru>
2014-11-30 12:13:56 +0300

    New abstracmodel

--- abstractmodel.py
+++ abstractmodel.py
@@ -13,10 +13,10 @@ class AbstractModel:
     def __init__(self, ml, model, team, logger, output):
         '''
         Parameters:
-            ml	model loader instance
-            model	model name
-            team	the unique team name
-                logger	model logger
+            ml  model loader instance
+            model   model name
+            team    the unique team name
+                logger  model logger
                 output  model output file
         '''
         self.ml = ml
@@ -29,24 +29,28 @@ class AbstractModel:
     def team_specific_num(self):
         return hash(self.team)
 
-    def team_arguments(self):
+    def team_arguments(self, input_params):
         '''
-        Run the simulation arguments
-        Random or based on team_specific_num
-            dict
+        Get the unique model arguments
+        Arguments:
+            model requirements: dict
+                    {'arg1': 1, 'arg2': 2}
+        Return value:
+            Random or based on team_specific_num dict
                     {'x': 1, 'y': 2}
         '''
+        self.ml.check_input_params(self.model, input_params)
         return {}
 
     def pre_production(self, input_params, model_params, components):
         '''
         Run the pre-production simulation
         Arguments:
-            dict
+            model requirements: dict
                     {'arg1': 1, 'arg2': 2}
-            dict
+            model parameters: dict
                     {'arg1': 1, 'arg2': 2}
-            dict
+            components: dict
                     {'component1': {'arg1': 1, 'arg2': 2},
                          'component2': {'arg3': 3, 'arg4': 4}}
         Return value:
@@ -62,15 +66,18 @@ class AbstractModel:
         self.ml.check_model_params(self.model, model_params)
         self.ml.check_components(self.model, components)
 
-    def production(self, interm_params, hidden_params, components):
+    def production(self, input_params, interm_params,
+                   hidden_params, components):
         '''
         Run the production simulation
         Arguments:
-            dict
+            model requirements: dict
+                    {'arg1': 1, 'arg2': 2}
+            intermediate parameters: dict
                     {'a1': 1, 'a2': 2}
-            dict
+            hidden intermediate parameters: dict
                     {'a3': 3, 'quality': 0.1}
-            dict
+            components: dict
                     {'component1': {'arg1': 1, 'arg2': 2},
                          'component2': {'arg3': 3, 'arg4': 4}}
         Return value:
@@ -81,5 +88,6 @@ class AbstractModel:
             CriticalError - unpredicted
 
         '''
+        self.ml.check_input_params(self.model, input_params)
         self.ml.check_interm_params(self.model, interm_params, hidden_params)
         self.ml.check_components(self.model, components)
